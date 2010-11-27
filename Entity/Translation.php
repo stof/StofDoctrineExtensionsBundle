e4cd9c721a9181e51e2ed9037d5c8bb5ee3c6e38
Christophe Coevoet <stof@notk.org>
2010-11-27 20:42:25 +0100

    Updated indexes to match the latest update of the vendor

--- Entity/Translation.php
+++ Entity/Translation.php
@@ -6,9 +6,15 @@ namespace Bundle\DoctrineExtensionsBundle\Entity;
  * Bundle\DoctrineExtensionsBundle\Entity\Translation
  *
  * @orm:Entity(repositoryClass="Gedmo\Translatable\Repository\TranslationRepository")
- * @orm:Table(name="ext_translation", indexes={
- *      @orm:index(name="lookup_idx", columns={"locale", "entity", "foreign_key", "field"})
- * })
+ * @orm:Table(
+ *         name="ext_translations",
+ *         indexes={@orm:index(name="translations_lookup_idx", columns={
+ *             "locale", "entity", "foreign_key"
+ *         })},
+ *         uniqueConstraints={@orm:UniqueConstraint(name="lookup_unique_idx", columns={
+ *             "locale", "entity", "foreign_key", "field"
+ *         })}
+ * )
  */
 class Translation extends AbstractTranslation
 {
