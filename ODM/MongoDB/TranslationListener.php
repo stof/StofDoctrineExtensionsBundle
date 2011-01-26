2ac9179f5b28a7b1c4efea091153b9f176031792
Justin Hileman <justin@justinhileman.info>
2011-01-26 11:39:19 -0500

    (lazily) set Translatable locale from the session.

--- ODM/MongoDB/TranslationListener.php
+++ ODM/MongoDB/TranslationListener.php
@@ -3,6 +3,7 @@
 namespace Stof\DoctrineExtensionsBundle\ODM\MongoDB;
 
 use Gedmo\Translatable\ODM\MongoDB\TranslationListener as BaseTranslationListener;
+use Symfony\Component\HttpFoundation\Session;
 
 /**
  * TranslationListener
@@ -12,4 +13,17 @@ use Gedmo\Translatable\ODM\MongoDB\TranslationListener as BaseTranslationListene
 class TranslationListener extends BaseTranslationListener
 {
     protected $defaultTranslationDocument = 'Stof\DoctrineExtensionsBundle\Document\Translation';
+
+    /**
+     * Set the translation listener locale from the session.
+     *
+     * @param Session $session
+     * @return void
+     */
+    public function setTranslatableLocaleFromSession(Session $session = null)
+    {
+        if ($session !== null) {
+            $this->setTranslatableLocale($session->getLocale());
+        }
+    }
 }
