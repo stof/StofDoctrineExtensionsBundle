55da7eb97ad6c91da30df1d69091bb557dd70ef8
jsor <jan@sorgalla.com>
2011-05-04 11:23:58 +0200

    Possibility to configure $translationFallback for the TranslationListener

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -26,6 +26,7 @@ class Configuration implements ConfigurationInterface
                     ->cannotBeEmpty()
                     ->defaultValue('en')
                 ->end()
+                ->booleanNode('translation_fallback')->end()
             ->end()
         ;
 
