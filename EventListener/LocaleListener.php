ef0d9a4cf2f3ac9dde94621b4de91d19d39655d7
Christophe Coevoet <stof@notk.org>
2012-02-11 13:01:49 +0100

    Replaced listeners by some Symfony listeners
    They hold a reference to the Gedmo listener to apply some logic on them.

---
+++ EventListener/LocaleListener.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Stof\DoctrineExtensionsBundle\EventListener;
+
+use Gedmo\Translatable\TranslatableListener;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpKernel\Event\GetResponseEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
+
+/**
+ * This listeners sets the current locale for the TranslatableListener
+ *
+ * @author Christophe COEVOET
+ */
+class LocaleListener implements EventSubscriberInterface
+{
+    private $translatableListener;
+
+    public function __construct(TranslatableListener $translatableListener)
+    {
+        $this->translatableListener = $translatableListener;
+    }
+
+    /**
+     * Set the translation listener locale from the request.
+     *
+     * This method should be attached to the kernel.request event.
+     *
+     * @param \Symfony\Component\HttpKernel\Event\GetResponseEvent $event
+     */
+    public function onKernelRequest(GetResponseEvent $event)
+    {
+        $this->translatableListener->setTranslatableLocale($event->getRequest()->getLocale());
+    }
+
+    static public function getSubscribedEvents()
+    {
+        return array(
+            KernelEvents::REQUEST => 'onKernelRequest',
+        );
+    }
+}
