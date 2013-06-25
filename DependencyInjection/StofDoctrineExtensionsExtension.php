6577f2388abeac4896aa0aef37fc1b6f2c6923e9
Christophe Coevoet <stof@notk.org>
2013-06-25 10:05:01 +0200

    Removed an unused use statement

--- DependencyInjection/StofDoctrineExtensionsExtension.php
+++ DependencyInjection/StofDoctrineExtensionsExtension.php
@@ -7,7 +7,6 @@ use Symfony\Component\Config\Definition\Processor;
 use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\DependencyInjection\Reference;
 
 class StofDoctrineExtensionsExtension extends Extension
 {
