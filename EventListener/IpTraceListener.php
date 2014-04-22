0f96ad38877fad2eac10b9c448dd0a4b4bf5544c
alterphp <pierre-charles@lephare.com>
2014-04-22 09:43:00 +0200

    Push again lost commit + No injection of Request service

--- EventListener/IpTraceListener.php
+++ EventListener/IpTraceListener.php
@@ -14,39 +14,27 @@ use Gedmo\IpTraceable\IpTraceableListener;
  *
  * @author Pierre-Charles Bertineau <pc.bertineau@alterphp.com>
  */
-class BlameListener implements EventSubscriberInterface
+class IpTraceListener implements EventSubscriberInterface
 {
-    /**
-     * @var Request
-     */
-    private $request;
-
     /**
      * @var IpTraceableListener
      */
     private $ipTraceableListener;
 
-    public function __construct(IpTraceableListener $ipTraceableListener, Request $request = null, )
+    public function __construct(IpTraceableListener $ipTraceableListener)
     {
         $this->ipTraceableListener = $ipTraceableListener;
-        $this->request = $request;
     }
 
     /**
-     * Set the username from the security context by listening on core.request
+     * Set client IP from event Request
      *
      * @param GetResponseEvent $event
      */
     public function onKernelRequest(GetResponseEvent $event)
     {
-        if (null === $this->request) {
-            return;
-        }
+        $ip = $event->request->getClientIp();
 
-        // TODO: New in 2.3, trusted proxies are defined in a parameter (kernel.trusted_proxies)
-        // $this->request->setTrustedProxies(array('127.0.0.1'));
-
-        $ip = $this->request->getClientIp();
         if (null !== $ip) {
             $this->ipTraceableListener->setIpValue($ip);
         }
