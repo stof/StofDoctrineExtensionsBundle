f2840a08915141d3bab29181d1ea2133122197e0
HypeMC <hypemc@gmail.com>
2025-09-23 04:11:03 +0200

    Add option to enable `SoftDeleteableListener` post-flush event

--- src/Resources/config/softdeleteable.php
+++ src/Resources/config/softdeleteable.php
@@ -11,6 +11,7 @@ return static function (ContainerConfigurator $container): void {
 
     $container->services()
         ->set('stof_doctrine_extensions.listener.softdeleteable', param('stof_doctrine_extensions.listener.softdeleteable.class'))
+            ->args([abstract_arg('Set in the extension')])
             ->call('setCacheItemPool', [service('stof_doctrine_extensions.metadata_cache')])
             ->call('setClock', [service('clock')->ignoreOnInvalid()])
             ->call('setAnnotationReader', [service('.stof_doctrine_extensions.reader')->ignoreOnInvalid()])
